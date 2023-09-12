d5e5c985f8b848b424583a8091ddd04c007668cc
Kumar Abhinav <68700523+kumabhinav@users.noreply.github.com>
2023-09-12 20:42:42 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is a java program");
+        System.out.println("output from java program");
+        System.out.println(new Date());
+    }
+}
